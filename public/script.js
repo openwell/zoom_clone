b572d3b4d26d7e1bfd05ae9af7ff5aecea28a858
Timi Ojo <openwell@Timis-MacBook-Pro.local>
2020-08-08 23:39:14 +0100

    visited user can join stream

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
+const socket = io('/');
+
 const myVideo = document.createElement('video');
 const videoGrid = document.getElementById('video-grid');
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
 
 let myVideoStream;
 navigator.mediaDevices
@@ -11,7 +19,33 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', function (call) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement('video');
+      call.on('stream', function (remoteStream) {
+        addVideoStream(video, remoteStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+
+peer.on('open', (id) => {
+  console.log(id, 'open');
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  console.log('new user', userId);
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
