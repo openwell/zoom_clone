e2af8d7e912ddf39265dcc14f858ae0ce338b6d0
Timi Ojo <openwell@Timis-MacBook-Pro.local>
2020-08-08 02:25:26 +0100

    dependency installation and server test

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const { v4: uuid } = require('uuid');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  //   res.render('rooms');
+  // res.status(200).send('Hello world')
+  res.redirect(`/${uuid()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+server.listen(3030, () => {
+  console.log('we live @ port 3030');
+});
